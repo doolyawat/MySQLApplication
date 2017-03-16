46dc11fc0f10fdfb08efaa57eaf654728933db9d
doolyawat <se57160685@gmail.com>
2017-03-16 10:24:33 +0700

    Initial commit

---
+++ app/src/main/java/com/krootoom/mysqlapplication/TodoList.java
@@ -0,0 +1,30 @@
+package com.krootoom.mysqlapplication;
+
+import java.io.Serializable;
+
+/**
+ * Created by doolyawat on 2/10/2017 AD.
+ */
+
+public class TodoList implements Serializable{
+    public int taskid;
+    public String taskname;
+
+    public int getTaskid() {
+        return taskid;
+    }
+
+    public void setTaskid(int taskid) {
+        this.taskid = taskid;
+    }
+
+    public String getTaskname() {
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+
+}
